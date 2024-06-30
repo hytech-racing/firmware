eafa26357d6782a12751123a1729a11ca0d56d70
walkermburns <walkermburns@gmail.com>
2024-06-30 11:52:09 -0700

    Fixing HyTechCAN lib diff

--- Libraries/HyTech_CAN/EM_status.h
+++ Libraries/HyTech_CAN/EM_status.h
@@ -16,16 +16,11 @@ public:
     inline void load(uint8_t buf[])           { memcpy(this, buf, sizeof(*this)); }
     inline void write(uint8_t buf[])    const { memcpy(buf, this, sizeof(*this)); }
 
-    //inline uint8_t get_voltage_gain()   const { return gain & 0xF;  }
-    inline uint8_t get_voltage_gain()   const { return ((gain & 0x1F) >> 1); }  // Modification per DBC file, yet to be verified
-    //inline uint8_t get_current_gain()   const { return gain >> 4;   }
-    inline uint8_t get_current_gain()   const { return (((gain & 0x01) << 3) | ((flags & 0xE0) >> 5)); }
-    //inline bool get_overvoltage()       const { return flags & 0x1; }
-    inline bool get_overvoltage()       const { return flags & 0x80; }
-    //inline bool get_overpower()         const { return flags & 0x2; }
-    inline bool get_overpower()         const { return flags & 0x40; }
-    //inline bool get_logging()           const { return flags & 0x4; }
-    inline bool get_logging()           const { return flags & 0x20; }
+    inline uint8_t get_voltage_gain()   const { return gain & 0xF;  }
+    inline uint8_t get_current_gain()   const { return gain >> 4;   }
+    inline bool get_overvoltage()       const { return flags & 0x1; }
+    inline bool get_overpower()         const { return flags & 0x2; }
+    inline bool get_logging()           const { return flags & 0x4; }
 
 #ifdef HT_DEBUG_EN
     void print() {
@@ -42,7 +37,6 @@ public:
 private:
     uint8_t gain;
     uint8_t flags; //@Parse @Flaglist(overvoltage, overpower, logging)
-    uint16_t dummy; // Compensate 4-byte data frame
 };
 
 #pragma pack(pop) 
